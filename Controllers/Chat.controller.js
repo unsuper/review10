0352ca83d92aee06fac81d5b9715bd10323363ae
baalzbarbarian <nguyenduchai.19021992@gmail.com>
2020-12-19 15:48:53 +0700

    update API

--- Controllers/Chat.controller.js
+++ Controllers/Chat.controller.js
@@ -30,19 +30,7 @@ exports._addChat = async (req, res) => {
 };
 
 exports._getAll = async (req, res) => {
-  let chatData = await Chat.find(
-    { movie_id: req.params.movie_id },
-    function (err, data) {
-      if (err) {
-        res.json({
-          result: false,
-          message: "get all chat fail ! " + err.message,
-          items: [],
-        });
-      }
-      return data;
-    }
-  );
+  let sort = Number(req.params.sort);
 
   let evaluateData = await Evaluate.find(
     { movie_id: req.params.movie_id },
@@ -59,6 +47,20 @@ exports._getAll = async (req, res) => {
     }
   );
 
+  let chatData = await Chat.find(
+    { movie_id: req.params.movie_id },
+    function (err, data) {
+      if (err) {
+        res.json({
+          result: false,
+          message: "get all chat fail ! " + err.message,
+          items: [],
+        });
+      }
+      return data;
+    }
+  ).sort({ "create_at": sort });
+
   let allData = [];
   let likeData = [];
   await chatData.map((val, ind) => {
@@ -69,18 +71,16 @@ exports._getAll = async (req, res) => {
           message: err,
         });
       }
-      if (data > 0) {
-        if (likeData.length === 0) {
-          likeData[0] = {
-            chat_id: val._id,
-            count: data,
-          };
-        } else {
-          likeData.push({
-            chat_id: val._id,
-            count: data,
-          });
-        }
+      if (likeData.length === 0) {
+        likeData[0] = {
+          chat_id: val._id,
+          count: data,
+        };
+      } else {
+        likeData.push({
+          chat_id: val._id,
+          count: data,
+        });
       }
     });
 
@@ -122,4 +122,100 @@ exports._getAll = async (req, res) => {
       }
     });
   });
+};
+
+exports._getAllByLike = async (req, res) => {
+  let allData = [];
+  let likeData = [];
+
+  let ratingData = await Evaluate.find(
+    { movie_id: req.params.movie_id },
+    function (err, data) {
+      if (err) {
+        res.json({
+          result: false,
+          message: "get all evalData fail ! " + err.message,
+          items: [],
+        });
+      } else {
+        return data;
+      }
+    }
+  );
+
+  await Chat.find({ movie_id: req.params.movie_id }, function (err, data) {
+    if (err) {
+      res.json({
+        result: false,
+        message: "get all chat fail ! " + err.message,
+        items: [],
+      });
+    } else {
+      data.map((val, ind) => {
+        Like.countDocuments({ chat_id: val._id }, function (err, count) {
+          if (err) {
+            res.json({
+              status: -1,
+              message: err,
+            });
+          } else {
+            likeData.push({
+              chat_id: val._id,
+              count: count,
+            });
+
+            User.findOne({ _id: val.user_id }, function (err, user) {
+              if (err) {
+                res.json({
+                  result: false,
+                  message: "get all chat fail ! " + err.message,
+                  items: [],
+                });
+              } else {
+                allData.push({
+                  chat_id: val._id,
+                  content: val.message,
+                  create_at: val.create_at,
+                  update_at: val.update_at,
+                  user_id: user._id,
+                  userInfo: user,
+                  sumLike: count,
+                });
+                if (ind === data.length - 1) {
+                  let sortArray = allData.sort(
+                    compareValues("sumLike", "desc")
+                  );
+                  res.json({
+                    status: true,
+                    data: sortArray,
+                    ratings: ratingData,
+                    countLike: likeData,
+                  });
+                }
+              }
+            });
+          }
+        });
+      });
+    }
+  });
+
+  function compareValues(key, order = "asc") {
+    return function (a, b) {
+      if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+        return 0;
+      }
+
+      const varA = typeof a[key] === "string" ? a[key].toUpperCase() : a[key];
+      const varB = typeof b[key] === "string" ? b[key].toUpperCase() : b[key];
+
+      let comparison = 0;
+      if (varA > varB) {
+        comparison = 1;
+      } else if (varA < varB) {
+        comparison = -1;
+      }
+      return order == "desc" ? comparison * -1 : comparison;
+    };
+  }
 };
--- Routes/Chat.route.js
+++ Routes/Chat.route.js
@@ -1,6 +1,7 @@
 module.exports = function (app) {
     const Chat = require("../Controllers/Chat.controller");
     app.route('/v1/chat/create').post(Chat._addChat)
-    app.route('/v3/chat/getAll/:movie_id').get(Chat._getAll)
+    app.route('/v3/chat/getAll/:movie_id/:sort').get(Chat._getAll)
+    app.route('/v3/chat/getByLike/:movie_id').get(Chat._getAllByLike)
   };
   
