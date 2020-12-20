e4d3c8a648e5fe17f2a8b9f167f21845207f986c
baalzbarbarian <nguyenduchai.19021992@gmail.com>
2020-12-20 20:03:53 +0700

    fix

--- Routes/countviews.route.js
+++ Routes/countviews.route.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
-    const views = require("../Controllers/sumviews.controller");
-    app.route("/v1/countview/:movie_id").get(views.countViewsByMovieId);
-    app.route("/v1/getViews/:movie_id").get(views.getViewsByMovieId);
+    const sumviews = require("../Controllers/sumviews.controller");
+    app.route("/v1/countview/:movie_id").get(sumviews.countViewsByMovieId);
+    app.route("/v1/getViews/:movie_id").get(sumviews.getViewsByMovieId);
   };
   
